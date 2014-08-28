f625d041bfec75e7e2b236f75c56df4060b60b49
Bartek Bulat <bartek@1000i.pl>
2014-08-28 12:54:59 +0200

    Add features section.

--- config/application.rb
+++ config/application.rb
@@ -16,9 +16,9 @@ module Trippro
   class Application < Rails::Application
     config.time_zone = 'Warsaw'
     config.i18n.enforce_available_locales = false
-    config.i18n.default_locale = :pl
+    config.i18n.default_locale = :en
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.available_locales = [:pl]
+    config.i18n.available_locales = [:en]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
