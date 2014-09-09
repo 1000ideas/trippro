16cadfd01a791bec32b52983bd7417b7e3459da9
Kamil Suwała <kamil@1000i.pl>
2014-09-09 14:37:30 +0200

    Niusy

---
+++ config/initializers/news_overwrite.rb
@@ -0,0 +1,7 @@
+L::News.class_eval do
+	has_attached_file :photo,
+	styles: { thumb: "50x50", small: "200x200>", medium: "800x600>", large: "1280x780" },
+	path: ":rails_root/public/system/news_photos/:id/:style/:filename",
+	url: "/system/news_photos/:id/:style/:filename",
+	preserve_files: true
+end
