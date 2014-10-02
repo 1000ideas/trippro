3ca8915068a683757cfd2ca89e5cb49f66816adf
Kamil Suwała <kamil@1000i.pl>
2014-10-02 10:53:30 +0200

    Added seeds for testimonials

---
+++ db/seeds/testimonials.rb
@@ -0,0 +1,15 @@
+Testimonial.destroy_all
+Testimonial.create(name: "Chris Dane",position: "President of Hickory Global Partners ", content: "We are partnering with TripPro® because their technologically 
+advanced platform and agent-focused design enables us to provide our member agencies airfares 
+to destinations worldwide presented as one-stop shopping,
+TripPro® enhances our already robust air program in an easy and smart way.")
+
+Testimonial.create(name: "Chris Dane",position: "President of Hickory Global Partners ", content: "We are partnering with TripPro® because their technologically 
+advanced platform and agent-focused design enables us to provide our member agencies airfares 
+to destinations worldwide presented as one-stop shopping,
+TripPro® enhances our already robust air program in an easy and smart way.")
+
+Testimonial.create(name: "Chris Dane",position: "President of Hickory Global Partners ", content: "We are partnering with TripPro® because their technologically 
+advanced platform and agent-focused design enables us to provide our member agencies airfares 
+to destinations worldwide presented as one-stop shopping,
+TripPro® enhances our already robust air program in an easy and smart way.")
