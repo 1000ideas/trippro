1cdff1ea108fd5f0defb78ec8cae094765fff042
Honorata <admin@1000i.pl>
2014-10-02 10:24:31 +0200

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ config.action_mailer.perform_deliveries = true;
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( jquery-1.7.js mercury.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
