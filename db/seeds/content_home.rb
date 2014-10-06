3ecbec2b433d45420e82f3f5f37472d0a328b1cf
Kamil Suwała <kamil@1000i.pl>
2014-10-06 16:16:19 +0200

    #8694 closed

--- db/seeds/content_home.rb
+++ db/seeds/content_home.rb
@@ -91,7 +91,93 @@ Content.create id: 'tour-content-video-17', value: "In an industry where custome
 													customer support to solve problems quickly, allowing agents to focus on their customers."
 Content.create id: 'tour-content-video-18', value: "Start using TripPro today, boost sales within weeks"
 
+#rozwijane sekcje
+#learn_book
+Content.create id: 'learn_book-content-1', value: "The Best Fares, Fast"
+Content.create id: 'learn_book-content-2', value: "More itineraries from multiple consolidators delivered
+ via the next-generation TripPro technology platform."
+Content.create id: 'learn_book-content-3', value: "Simple Interface"
+Content.create id: 'learn_book-content-4', value: "Works seamlessly delivering multiple consolidator fares
+ alongside your GDS screen or web browser"
+Content.create id: 'learn_book-content-5', value: "Instant Booking"
+Content.create id: 'learn_book-content-6', value: "Connect to TripPro and confirm seats and book instantly
+ with one click. It’s that simple!"
+Content.create id: 'learn_book-content-7', value: "Agent Controlled Markup"
+Content.create id: 'learn_book-content-8', value: "Fares from the world’s leading consolidators, available instantly."
+Content.create id: 'learn_book-content-9', value: "Single Source Platform"
+Content.create id: 'learn_book-content-10', value: "Directly control commissions and markup to stay competitive and increase profits"
+
+Content.create id: 'quote-content-1', value: "“We are partnering with TripPro® because their technologically advanced 
+platform and agent-focused design enables us to provide our member agencies 
+airfares to destinations worldwide presented as one-stop shopping, TripPro® enhances 
+our already robust air program in an easy and smart way.”"
+
+
+Content.create id: 'benefits-content-1', value:"Our product, your benefits"
+Content.create id: 'benefits-content-2', value:"Maximize Profits"
+Content.create id: 'benefits-content-3', value:"Complete control over commission and 
+profit with one click access to unparalleled consolidator inventory."
+Content.create id: 'benefits-content-4', value:"Easy to Use"
+Content.create id: 'benefits-content-5', value:"Fares displayed seamlessly via browser
+ or with anypreferred GDS partner on the same screen, no training needed."
+Content.create id: 'benefits-content-6', value:"Saves Time"
+Content.create id: 'benefits-content-7', value:"One-click booking eliminates manual
+processing time and reduces new agent training time."
+Content.create id: 'benefits-content-8', value:"Price Assurance"
+Content.create id: 'benefits-content-9', value:"Fares from the world’s leading
+consolidators, available instantly."
+Content.create id: 'benefits-content-10', value:"Experience and Trust"
+Content.create id: 'benefits-content-11', value:"Built for travel professionals by travel experts with 
+over 120 years of experience helping agencies grow."
+
+#QC
+
+Content.create id: 'qc-content-1', value:"30% increase"
+Content.create id: 'qc-content-2', value:"ticket processing time"
+Content.create id: 'qc-content-3', value:"20%"
+Content.create id: 'qc-content-4', value:"reduction of errors"
+Content.create id: 'qc-content-5', value:"120"
+Content.create id: 'qc-content-6', value:"years of industry"
+Content.create id: 'qc-content-7', value:"24/7"
+Content.create id: 'qc-content-8', value:"Support"
+
+
+Content.create id: 'qc-content-9', value:"Instant Ticketing"
+Content.create id: 'qc-content-10', value:"Connect to TripPro and confirm seats and 
+ticket instantly with one click. It’s that simple!"
+Content.create id: 'qc-content-11', value:"Intuitive Interface"
+Content.create id: 'qc-content-12', value:"Works seamlessly delivering multiple consolidator 
+fares alongside your GDS screen or web browser."
+Content.create id: 'qc-content-13', value:"Single Source Platform"
+Content.create id: 'qc-content-14', value:"Search exclusive global content from your choice of Mondee’s four leading
+ consolidators and multiple GDS from a single screen."
+Content.create id: 'qc-content-15', value:"24/7 support"
+Content.create id: 'qc-content-16', value:"Customer support available around 
+the clock to solve problems quickly."
+Content.create id: 'qc-content-17', value:"Maximize profits"
+Content.create id: 'qc-content-18', value:"Wasteful ticketing processing time 
+is reduced, allowing agents to focus on increasing ticket sales"
+Content.create id: 'qc-content-19', value:"Satisfied Customers"
+Content.create id: 'qc-content-20', value:"More time with customers creates happier, 
+loyal customers that return"
+Content.create id: 'qc-content-21', value:"Time Savings"
+Content.create id: 'qc-content-22', value:"Agencies can focus on building strong customer 
+relationship instead of manually processing tickets"
+Content.create id: 'qc-content-23', value:"Experience and Trust"
+Content.create id: 'qc-content-24', value:"Built for travel professionals by travel experts with
+ over 120 years of experience helping agencies grow"
 puts "Tour done"
+
+#reporting
+
+Content.create id: 'report-content-1', value:"Cross-platform Availability"
+Content.create id: 'report-content-2', value:"TripPro is available on all major platforms"
+Content.create id: 'report-content-3', value:"TripPro &reg; API"
+Content.create id: 'report-content-4', value:"Api enables travel agencies to develop their own travel
+ products, rather than  using off-the-shelf solutions."
+Content.create id: 'report-content-5', value:"TripPro&reg; DESKTOP"
+Content.create id: 'report-content-6', value:"30% increase"
+
 
 #Contact
 Content.create id: 'contact-content-1', value: "Contact Us"
