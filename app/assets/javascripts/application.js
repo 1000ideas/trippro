d23c733809e95e472442d03317afeabe44962231
Marcin Szmigiel <marcin@1000i.pl>
2015-09-08 14:40:10 +0200

    code for live chat

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,6 +30,25 @@ abide : {
 }
 }); });
 
+function initLiveChat() {
+  var se = document.createElement('script');
+  se.type = 'text/javascript';
+  se.async = true;
+  se.src = '//storage.googleapis.com/code.snapengage.com/js/8bfe0661-fc94-45f8-934e-b51a11627bbe.js';
+  var done = false;
+  se.onload = se.onreadystatechange = function() {
+    if (!done && (!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete')) {
+      done = true;
+      /* Place your SnapEngage JS API code below */
+      /* SnapEngage.allowChatSound(true); Example JS API: Enable sounds for Visitors. */
+     /* SnapABug.setUserEmail(emailId);*/
+    }
+  };
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(se, s);
+};
+
+
 $(document).ready(function(){
 	
 	$(window).scroll(function(){
@@ -44,5 +63,9 @@ $(document).ready(function(){
 		$('html, body').animate({scrollTop : 0},800);
 		return false;
 	});
-	
+
+  $('#live-chat-button').click(function() {
+    initLiveChat();
+  })
+
 });
